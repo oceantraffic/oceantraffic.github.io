104ce51d673e4c5c4b21f9f5fc78ea9fe500ea16
oceantraffic <vadim.ivanyuk.126@gmail.com>
2020-08-03 12:38:30 +0300

    Updates

---
+++ precache-manifest.2fe7cf7026ca4e261a93248412967d5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303b223a4a60ad61c0e2c4b8b4db5d44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70628fd4914e4c337042",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3d4547624884ae5e4b4b",
+    "url": "/static/css/main.80f9aba4.chunk.css"
+  },
+  {
+    "revision": "70628fd4914e4c337042",
+    "url": "/static/js/2.64e3e811.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.64e3e811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4547624884ae5e4b4b",
+    "url": "/static/js/main.17e9f68b.chunk.js"
+  },
+  {
+    "revision": "cf185b119b823a50d682",
+    "url": "/static/js/runtime-main.c452e221.js"
+  }
+]);
